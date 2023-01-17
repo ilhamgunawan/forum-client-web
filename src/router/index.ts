4bfd2ab2e4496ffd81ff4e52213430be2cdc47c9
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-01-17 15:58:07 +0700

    Init Sign In Page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 Vue.use(VueRouter);
 
@@ -9,17 +8,9 @@ const router = new VueRouter({
   base: import.meta.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/auth/signin',
+      name: 'auth/signin',
+      component: () => import('../views/AuthSignInView.vue'),
     },
   ],
 });
---
+++ src/views/AuthSignInView.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="h-screen flex items-center justify-center">
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title">Sign In</h2>
+        <form class="flex flex-col gap-4">
+          <div class="form-control w-full max-w-xs">
+            <label for="username" class="pb-1 label-text">Username</label>
+            <input id="username" type="text" placeholder="Type username here" class="input input-bordered w-full max-w-xs" />
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label for="password" class="pb-1 label-text">Password</label>
+            <input id="password" type="password" placeholder="Type password here" class="input input-bordered w-full max-w-xs" />
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <button type="submit" class="btn btn-primary">Sign In</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </main>
+</template>
