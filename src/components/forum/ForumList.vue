792b1f8d0686007d607d1d439e69eeee2e629ba8
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-01-23 16:30:18 +0700

    Forums

---
+++ src/components/forum/ForumList.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import type { Forum } from '@/lib/forum';
+import ForumItem from './ForumItem.vue';
+
+export default {
+  components: { ForumItem },
+  props: {
+    forums: {
+      type: Array<Forum>,
+      required: true,
+    },
+  },
+  watch: {
+    forums(newValue: Forum[]) {
+      console.log('forums: ', newValue);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="w-full flex flex-col gap-4 p-4">
+    <ForumItem
+      v-for="forum in $props.forums"
+      v-bind:key="forum.id"
+      v-bind:forum="forum"
+    />
+  </div>
+</template>
